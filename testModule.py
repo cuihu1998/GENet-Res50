f0852a474eed9fac9275903c998c4eb812658a56
cuihu1998 <31977927+cuihu1998@users.noreply.github.com>
2021-05-29 09:13:19 +0800

    Delete testModule.py

--- testModule.py
+++
@@ -1,8 +0,0 @@
-
-import mindspore
-import numpy as np
-from src.GENet import *
-input = mindspore.Tensor(np.ones([128,3,224,224]).astype(np.float32))
-net = GE_resnet50()
-out = net(input)
-print(out.shape)
